0e99f0c96888de14511a3eaf55a75347e3f5e43d
Ivan Mudrák <xmudra03@vutbr.cz>
2024-10-26 22:13:45 +0200

    code clean up

--- thirst_quest/lib/widgets/location_map.dart
+++ thirst_quest/lib/widgets/location_map.dart
@@ -1,13 +1,13 @@
 import 'dart:async';
-
 import 'package:flutter/material.dart';
 import 'package:flutter_map/flutter_map.dart';
 import 'package:geolocator/geolocator.dart';
 import 'package:latlong2/latlong.dart';
-import 'package:shared_preferences/shared_preferences.dart';
 import 'package:thirst_quest/api/models/water_bubbler.dart';
 import 'package:thirst_quest/services/water_bubbler_service.dart';
-import 'package:url_launcher/url_launcher.dart';
+import 'package:thirst_quest/widgets/map_control_buttons.dart';
+import 'package:thirst_quest/widgets/map_widget.dart';
+import 'package:thirst_quest/services/location_service.dart';
 
 class LocationMap extends StatefulWidget {
   const LocationMap({super.key});
@@ -17,84 +17,61 @@ class LocationMap extends StatefulWidget {
 }
 
 class LocationMapState extends State<LocationMap> {
-  LatLng _currentPosition = const LatLng(49.2273106, 16.5983539); // VUT FIT
+  LatLng _currentPosition =
+      const LatLng(49.2273106, 16.5983539); // Default position (VUT FIT)
   final MapController _mapController = MapController();
   StreamSubscription<Position>? _positionStream;
   bool trackPosition = true;
   final WaterBubblerService _waterBubblerService = WaterBubblerService();
+  final LocationService _locationService = LocationService();
   Timer? _debounceTimer;
   List<WaterBubbler> _waterBubblers = [];
 
   @override
   void initState() {
     super.initState();
-    _getCurrentLocation();
+    _initializeLocation();
   }
 
   @override
   void dispose() {
-    SharedPreferences.getInstance().then((pref) {
-      pref.setDouble('lastLatitude', _currentPosition.latitude);
-      pref.setDouble('lastLongitude', _currentPosition.longitude);
-    });
+    _locationService.saveLastKnownPosition(_currentPosition);
     _positionStream?.cancel();
+    _debounceTimer?.cancel();
     super.dispose();
   }
 
-  Future<void> _getCurrentLocation() async {
-    bool serviceEnabled;
-    LocationPermission permission;
-
-    SharedPreferences.getInstance().then((pref) {
-      double? lastLatitude = pref.getDouble('lastLatitude');
-      double? lastLongitude = pref.getDouble('lastLongitude');
-      if (lastLatitude != null && lastLongitude != null) {
-        _currentPosition = LatLng(lastLatitude, lastLongitude);
-      }
-    });
-
-    serviceEnabled = await Geolocator.isLocationServiceEnabled();
-    if (!serviceEnabled) {
-      return;
+  Future<void> _initializeLocation() async {
+    LatLng? lastKnownPosition = await _locationService.getLastKnownPosition();
+    if (lastKnownPosition != null) {
+      setState(() {
+        _currentPosition = lastKnownPosition;
+        _mapController.move(_currentPosition, 13.0);
+      });
     }
 
-    // Check for location permissions
-    permission = await Geolocator.checkPermission();
-    if (permission == LocationPermission.denied) {
-      permission = await Geolocator.requestPermission();
-      if (permission == LocationPermission.denied) {
-        return;
-      }
+    if (await _locationService.isLocationServiceEnabled()) {
+      _startLocationUpdates();
     }
-
-    if (permission == LocationPermission.deniedForever) {
-      // Permissions are denied forever, don't continue
-      return;
-    }
-
-    // Get the current location
-    _startLocationUpdates();
   }
 
   void _startLocationUpdates() {
-    _positionStream = Geolocator.getPositionStream(
-            locationSettings: LocationSettings(
-                distanceFilter: 2)) // Update the location every 2 meters
-        .listen((Position position) => setState(() {
-              _currentPosition = LatLng(position.latitude, position.longitude);
-              if (trackPosition) {
-                _mapController.move(_currentPosition, 15.0);
-              }
-            }));
+    _positionStream =
+        _locationService.getPositionStream().listen((Position position) {
+      setState(() {
+        _currentPosition = LatLng(position.latitude, position.longitude);
+        if (trackPosition) {
+          _mapController.move(_currentPosition, 15.0);
+        }
+      });
+    });
   }
 
   void _onPositionChanged(MapCamera position, bool hasGesture) {
     trackPosition = false;
 
-    // Cancel the previous timer if it is still active
     _debounceTimer?.cancel();
 
-    // Start a new timer
     _debounceTimer = Timer(const Duration(milliseconds: 500), () async {
       final waterBubblers = await _waterBubblerService
           .getWaterBubblersByBBox(position.visibleBounds);
@@ -109,68 +86,22 @@ class LocationMapState extends State<LocationMap> {
     return Scaffold(
       body: Stack(
         children: [
-          FlutterMap(
+          MapWidget(
+            initialPosition: _currentPosition,
             mapController: _mapController,
-            options: MapOptions(
-              initialCenter: _currentPosition,
-              initialZoom: 15,
-              onPositionChanged: _onPositionChanged,
-            ),
-            children: [
-              TileLayer(
-                tileProvider: NetworkTileProvider(),
-                urlTemplate: 'https://tile.openstreetmap.org/{z}/{x}/{y}.png',
-                userAgentPackageName: 'com.example.app',
-              ),
-              MarkerLayer(
-                markers: [
-                  // Show the marker only if the location is available
-                  if (_positionStream != null)
-                    Marker(
-                      width: 50.0,
-                      height: 50.0,
-                      point: _currentPosition,
-                      child: Icon(
-                        Icons.my_location,
-                        color: Theme.of(context).colorScheme.primary,
-                        size: 40.0,
-                      ),
-                    ),
-                ],
-              ),
-              RichAttributionWidget(
-                attributions: [
-                  TextSourceAttribution(
-                    'OpenStreetMap contributors',
-                    onTap: () => launchUrl(
-                        Uri.parse('https://openstreetmap.org/copyright')),
-                  ),
-                ],
-              ),
-            ],
+            onPositionChanged: _onPositionChanged,
+            waterBubblers: _waterBubblers,
+            currentPosition: _currentPosition,
+            showPositionMarker: _positionStream != null,
           ),
-          Positioned(
-              top: 16.0,
-              right: 16.0,
-              child: Column(
-                children: [
-                  IconButton(
-                    iconSize: 25.0,
-                    color: Colors.black,
-                    onPressed: () => _mapController.rotate(0.0),
-                    icon: const Icon(Icons.explore_outlined),
-                  ),
-                  IconButton(
-                    iconSize: 25.0,
-                    color: Colors.black,
-                    onPressed: () {
-                      _mapController.moveAndRotate(_currentPosition, 15.0, 0.0);
-                      trackPosition = true;
-                    },
-                    icon: const Icon(Icons.my_location),
-                  )
-                ],
-              )),
+          ControlButtons(
+            mapController: _mapController,
+            currentPosition: _currentPosition,
+            onCenterButtonPressed: () {
+              _mapController.moveAndRotate(_currentPosition, 15.0, 0.0);
+              trackPosition = true;
+            },
+          ),
         ],
       ),
     );
---
+++ thirst_quest/lib/widgets/map_widget.dart
@@ -0,0 +1,79 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_map/flutter_map.dart';
+import 'package:latlong2/latlong.dart';
+import 'package:thirst_quest/api/models/water_bubbler.dart';
+import 'package:url_launcher/url_launcher.dart';
+
+class MapWidget extends StatelessWidget {
+  final LatLng initialPosition;
+  final MapController mapController;
+  final Function(MapCamera, bool) onPositionChanged;
+  final List<WaterBubbler> waterBubblers;
+  final LatLng currentPosition;
+  final bool showPositionMarker;
+
+  const MapWidget({
+    super.key,
+    required this.initialPosition,
+    required this.mapController,
+    required this.onPositionChanged,
+    required this.waterBubblers,
+    required this.currentPosition,
+    this.showPositionMarker = true,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return FlutterMap(
+      mapController: mapController,
+      options: MapOptions(
+        initialCenter: initialPosition,
+        initialZoom: 15,
+        onPositionChanged: onPositionChanged,
+      ),
+      children: [
+        TileLayer(
+          tileProvider: NetworkTileProvider(),
+          urlTemplate: 'https://tile.openstreetmap.org/{z}/{x}/{y}.png',
+          userAgentPackageName: 'com.example.app',
+        ),
+        MarkerLayer(
+          markers: [
+            if (showPositionMarker)
+              Marker(
+                width: 50.0,
+                height: 50.0,
+                point: currentPosition,
+                child: Icon(
+                  Icons.my_location,
+                  color: Theme.of(context).colorScheme.primary,
+                  size: 40.0,
+                ),
+              ),
+            for (final waterBubbler in waterBubblers)
+              Marker(
+                width: 50.0,
+                height: 50.0,
+                point: LatLng(waterBubbler.latitude, waterBubbler.longitude),
+                child: const Icon(
+                  Icons.local_drink,
+                  color: Colors.blue,
+                  size: 40.0,
+                ),
+              ),
+          ],
+        ),
+        RichAttributionWidget(
+          attributions: [
+            TextSourceAttribution(
+              'OpenStreetMap contributors',
+              onTap: () => launchUrl(
+                Uri.parse('https://openstreetmap.org/copyright'),
+              ),
+            ),
+          ],
+        ),
+      ],
+    );
+  }
+}
