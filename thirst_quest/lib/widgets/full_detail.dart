223543279f28bc689df7fe7729f7924aec2e29cd
Pregros <pregroscz@seznam.cz>
2024-11-19 14:25:40 +0100

    Added swipe down gesture to full detail and handle

--- thirst_quest/lib/widgets/full_detail.dart
+++ thirst_quest/lib/widgets/full_detail.dart
@@ -6,11 +6,10 @@ import 'package:thirst_quest/utils/distance_convertor.dart';
 class FullDetail extends StatelessWidget {
   final VoidCallback onClose;
 
-  const FullDetail(
-      {required this.onClose, super.key});
+  const FullDetail({required this.onClose, super.key});
 
   @override
-    Widget build(BuildContext context) {
+  Widget build(BuildContext context) {
     final screenWidth = MediaQuery.of(context).size.width;
     final screenHeight = MediaQuery.of(context).size.height;
     final mapState = context.watch<BubblerMapState>();
@@ -31,93 +30,104 @@ class FullDetail extends StatelessWidget {
         ],
       ),
       child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        mainAxisSize: MainAxisSize.min,
         children: [
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-              Row(
-                children: [
-                  Container(
-                    padding: EdgeInsets.all(8),
+          GestureDetector(
+            onVerticalDragEnd: (details) {
+              if (details.primaryVelocity! > 0) {
+                onClose();
+              }
+            },
+            child: Column(
+              children: [
+                // Drag Handle
+                Center(
+                  child: Container(
+                    width: 50,
+                    height: 5,
+                    margin: EdgeInsets.only(bottom: 16),
                     decoration: BoxDecoration(
-                      color: Colors.green,
-                      shape: BoxShape.circle,
-                    ),
-                    child: Text(
-                      '#',
-                      style: TextStyle(
-                        fontSize: 16,
-                        fontWeight: FontWeight.bold,
-                        color: Colors.white,
-                      ),
+                      color: Colors.grey,
+                      borderRadius: BorderRadius.circular(2.5),
                     ),
                   ),
-                  SizedBox(width: 8),
-                  Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Text(
-                        selectedBubbler.name ?? 'Bubbler Details',
-                        style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
-                      ),
-                      SizedBox(height: 4),
-                      Text(
-                        distanceToDisplay(selectedBubbler.distanceTo(currentLocation)),
-                        style: TextStyle(fontSize: 16, color: Colors.grey[600]),
-                      ),
-                    ],
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Row(
+                      children: [
+                        SizedBox(width: 8),
+                        Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text(
+                              selectedBubbler.name ?? 'Bubbler Details',
+                              style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+                            ),
+                            SizedBox(height: 4),
+                            Text(
+                              distanceToDisplay(
+                                selectedBubbler.distanceTo(currentLocation),
+                              ),
+                              style: TextStyle(
+                                fontSize: 16,
+                                color: Colors.grey[600],
+                              ),
+                            ),
+                          ],
+                        ),
+                      ],
+                    ),
+                    Row(
+                      children: [
+                        GestureDetector(
+                          onTap: () {
+                            print("Settings");
+                          },
+                          child: Icon(Icons.settings, color: Colors.grey, size: 32),
+                        ),
+                        SizedBox(width: 24),
+                        GestureDetector(
+                          onTap: () {
+                            print("Delete");
+                          },
+                          child: Icon(Icons.delete, color: Colors.red, size: 32),
+                        ),
+                        SizedBox(width: 24),
+                        GestureDetector(
+                          onTap: () {
+                            print("Directions");
+                          },
+                          child: Icon(Icons.directions, color: Colors.green, size: 32),
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+                SizedBox(height: 16),
+                Container(
+                  height: 150,
+                  width: screenWidth,
+                  color: Colors.grey[200],
+                  child: Center(
+                    child: Image.network(
+                      'https://via.placeholder.com/150', // TODO: Placeholder image
+                      height: 200,
+                      fit: BoxFit.cover,
+                    ),
                   ),
-                ],
-              ),
-              Row(
-                children: [
-                  GestureDetector(
-                    onTap: () {
-                      print("Settings");
-                    },
-                    child: Icon(Icons.settings, color: Colors.grey, size: 32),
-                  ),
-                  SizedBox(width: 24),
-                  GestureDetector(
-                    onTap: () {
-                      print("Delete");
-                    },
-                    child: Icon(Icons.delete, color: Colors.red, size: 32),
-                  ),
-                  SizedBox(width: 24),
-                  GestureDetector(
-                    onTap: () {
-                      print("Directions");
-                    },
-                    child: Icon(Icons.directions, color: Colors.green, size: 32),
-                  ),
-                ],
-              ),
-            ],
-          ),
-          SizedBox(height: 16),
-          Container(
-            height: 150,
-            width: screenWidth,
-            color: Colors.grey[200],
-            child: Center(
-              child: Image.network(
-                'https://via.placeholder.com/150',//selectedBubbler.photos[0].url, // TODO: add placeholder
-                height: 200,
-                fit: BoxFit.cover,
-              ),
-            )
-          ),
-          SizedBox(height: 16),
-          Text(
-            selectedBubbler.description ?? 'No description available.',
-            style: TextStyle(fontSize: 16),
-          ),
-          TextButton(
-            onPressed: () {},
-            child: Text('Read more'),
+                ),
+                SizedBox(height: 16),
+                Text(
+                  selectedBubbler.description ?? 'No description available.',
+                  style: TextStyle(fontSize: 16),
+                ),
+                TextButton(
+                  onPressed: () {},
+                  child: Text('Read more'),
+                ),
+              ],
+            ),
           ),
         ],
       ),
