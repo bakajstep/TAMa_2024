ea24f80e23ac41e1febe5b677c2dfbea5e680df5
Pregros <pregroscz@seznam.cz>
2024-12-10 00:29:21 +0100

    Added popupMenuButton, delete from full detail with listener

--- thirst_quest/lib/widgets/maintain_bubbler_button.dart
+++ thirst_quest/lib/widgets/maintain_bubbler_button.dart
@@ -1,10 +1,10 @@
 import 'package:flutter/material.dart';
-import 'package:maps_launcher/maps_launcher.dart';
 import 'package:provider/provider.dart';
 import 'package:thirst_quest/api/models/water_bubbler.dart';
-import 'package:thirst_quest/main.dart';
+import 'package:thirst_quest/di.dart';
+import 'package:thirst_quest/notifications/bubblers_reload.dart';
+import 'package:thirst_quest/services/water_bubbler_service.dart';
 import 'package:thirst_quest/states/global_state.dart';
-import 'package:thirst_quest/states/models/identity.dart';
 
 class MaintainBubblerButton extends StatelessWidget {
   final WaterBubbler waterBubbler;
@@ -15,11 +15,18 @@ class MaintainBubblerButton extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final globalState = context.watch<GlobalState>();
-    // final Identity? identity = globalState.user.identity;
+    final WaterBubblerService bubblerService = DI.get<WaterBubblerService>();
+
+    void deleteBubbler(String bubblerId) async {
+      await bubblerService.deleteWaterBubbler(bubblerId);
+      if (context.mounted) {
+        BubblersReload().dispatch(context);
+      }
+  }
+
     final String role = globalState.user.identity == null ? "" : globalState.user.identity!.roles[0];
     final String? bubblerOwnerId = waterBubbler.userId;
     final String? userId = role == "" ? null : globalState.user.identity!.id;
-
     final invisibleCondition = (role == "ROLE_ADMIN") || (userId == bubblerOwnerId);
 
     if (!invisibleCondition) return SizedBox.shrink();
@@ -27,30 +34,67 @@ class MaintainBubblerButton extends StatelessWidget {
     return Material(
       shape: const CircleBorder(),
       color: Colors.black.withOpacity(0.1),
-      child: IconButton(
-        onPressed: null,
-        iconSize: size != null ? (size!) : null,
-        padding: EdgeInsets.all(5),
-        constraints: BoxConstraints(),
-        style: const ButtonStyle(tapTargetSize: MaterialTapTargetSize.shrinkWrap),
+      child: PopupMenuButton<int>(
         icon: const Icon(Icons.settings_sharp, color: Colors.black),
+        iconSize: 24.0, 
+        padding: const EdgeInsets.all(5),
+        constraints: const BoxConstraints(),
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(8),
+        ),
+        onSelected: (int value) {
+          if (value == 1) {
+            // TODO: Redirect to bubbbler edit page
+          } else if (value == 2) {
+            showDialog(
+              context: context,
+              builder: (BuildContext context) {
+                return AlertDialog(
+                  title: const Text('Confirm Delete'),
+                  content: const Text('Are you sure you want to delete this bubbler?'),
+                  actions: [
+                    TextButton(
+                      onPressed: () {
+                        Navigator.of(context).pop();
+                      },
+                      child: const Text('Cancel'),
+                    ),
+                    TextButton(
+                      onPressed: () {
+                        deleteBubbler(waterBubbler.id!);
+                        Navigator.of(context).pop();
+                      },
+                      child: const Text('Delete'),
+                    ),
+                  ],
+                );
+              },
+            );
+          }
+        },
+        itemBuilder: (BuildContext context) => [
+          PopupMenuItem<int>(
+            value: 1,
+            child: Row(
+              children: const [
+                Icon(Icons.edit, color: Colors.black),
+                SizedBox(width: 8),
+                Text('Edit'),
+              ],
+            ),
+          ),
+          PopupMenuItem<int>(
+            value: 2,
+            child: Row(
+              children: const [
+                Icon(Icons.delete, color: Colors.black),
+                SizedBox(width: 8),
+                Text('Delete'),
+              ],
+            ),
+          ),
+        ],
       ),
     );
-
-    // return Container(
-    //   decoration: BoxDecoration(
-    //     color: Colors.grey[400], // Background color
-    //     shape: BoxShape.circle, // Makes the background circular
-    //   ),
-    //   child: IconButton(
-    //     onPressed: () => MapsLauncher.launchCoordinates(
-    //       waterBubbler.latitude,
-    //       waterBubbler.longitude,
-    //       waterBubbler.name,
-    //     ),
-    //     iconSize: size != null ? (size! - 20) : 24,
-    //     icon: const Icon(Icons.settings_sharp, color: Colors.white),
-    //   ),
-    // );
   }
 }
