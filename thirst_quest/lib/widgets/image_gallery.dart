3569310597bb3864d6ccb752b081d94822cc5471
Ivan Mudrák <xmudra03@vutbr.cz>
2024-12-10 19:12:03 +0100

    Fix image gallery scroll

--- thirst_quest/lib/widgets/image_gallery.dart
+++ thirst_quest/lib/widgets/image_gallery.dart
@@ -11,26 +11,25 @@ class ImageGallery extends StatelessWidget {
   Widget build(BuildContext context) {
     return Padding(
       padding: EdgeInsets.all(10.0),
-      child: SizedBox(
-        height: 350,
-        child: StaggeredGridView.countBuilder(
-          crossAxisCount: 3,
-          itemCount: imageUrls.length,
-          itemBuilder: (BuildContext context, int index) {
-            return ClipRRect(
-              borderRadius: BorderRadius.circular(10),
-              child: Container(
-                decoration: BoxDecoration(
-                  color: Colors.grey.shade300,
-                ),
-                child: NetworkImageWithLoading(imageUrl: imageUrls[index]),
+      child: StaggeredGridView.countBuilder(
+        crossAxisCount: 3,
+        itemCount: imageUrls.length,
+        itemBuilder: (BuildContext context, int index) {
+          return ClipRRect(
+            borderRadius: BorderRadius.circular(10),
+            child: Container(
+              decoration: BoxDecoration(
+                color: Colors.grey.shade300,
               ),
-            );
-          },
-          staggeredTileBuilder: (int index) => StaggeredTile.count(1, index.isEven ? 1.5 : 1),
-          mainAxisSpacing: 4.0,
-          crossAxisSpacing: 4.0,
-        ),
+              child: NetworkImageWithLoading(imageUrl: imageUrls[index]),
+            ),
+          );
+        },
+        staggeredTileBuilder: (int index) => StaggeredTile.count(1, index.isEven ? 1.5 : 1),
+        mainAxisSpacing: 4.0,
+        crossAxisSpacing: 4.0,
+        shrinkWrap: true,
+        physics: NeverScrollableScrollPhysics(),
       ),
     );
   }
